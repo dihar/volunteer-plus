a4e4d9c0f129dd904e82cc231653ab36a40fa1ef
dihar <diharin@gmail.com>
2019-09-28 00:14:12 +0300

    Updates

---
+++ precache-manifest.2552674c94abe1034ac2ff1c3904e499.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8125a44abe0452434b79819ab47eb6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1269698d348d8f1fe723",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "80a5c1457a0cee974e9b",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "1269698d348d8f1fe723",
+    "url": "./static/js/2.f49497be.chunk.js"
+  },
+  {
+    "revision": "80a5c1457a0cee974e9b",
+    "url": "./static/js/main.c647b797.chunk.js"
+  },
+  {
+    "revision": "fc571f747102d036ccb5",
+    "url": "./static/js/runtime-main.37a108eb.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
